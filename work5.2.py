43ae905987da392e257383e04b5bf35b31bf6b75
Zver <b.valeriya.andreevna@gmail.com>
2025-01-16 22:13:13 +0300

    vladik....

---
+++ work5.2.py
@@ -0,0 +1,34 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 0 < new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+
+            print("Такого этажа не существует")
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return self.name
+
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
+
+#не понимаю почему не выводит так же как и в задание Название: ЖК Эльбрус, кол-во этажей: 10
+#Название: ЖК Акация, кол-во этажей: 20 но ведь в целом это же работает)))))))

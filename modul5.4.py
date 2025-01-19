0d617c238a166ebe873e395811ad3d06a6814b5c
Zver <b.valeriya.andreevna@gmail.com>
2025-01-19 20:37:26 +0300

    для тебя вселенной целой будет мало.. ты орхидея y beauti flower..

---
+++ modul5.4.py
@@ -0,0 +1,76 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        instance = object.__new__(cls)
+        args = args[0]
+        cls.houses_history.append(args)
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        if isinstance(number_of_floors, House):
+            self.houses_history = number_of_floors.append()
+
+
+
+    def go_to(self, new_floor):
+        if 0 < new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+    def __del__(self):
+        return print(f'{self.name} снесён, но остался в истории..')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __it__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, other):
+        new_number = self.number_of_floors
+        if isinstance(other, House):
+            new_number += other.number_of_floors
+        elif isinstance(other, int):
+            new_number += other
+        else:
+            return NotImplemented
+        return House(self.number_of_floors, new_number)
+
+    def  __radd__(self, other):
+        return self.__add__(other)
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
